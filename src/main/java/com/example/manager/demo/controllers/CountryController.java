5c317d5abdd9aee546e6096c4595128b00e3278a
Gitari65 <agitari65@gmail.com>
2023-10-05 12:42:40 +0300

    country  controllers

--- src/main/java/com/example/manager/demo/controllers/CountryController.java
+++ src/main/java/com/example/manager/demo/controllers/CountryController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CountryController {
     @GetMapping("/countries")
-    public String getCountry(){
+    public String goCountry(){
         return "country";
     }
 
