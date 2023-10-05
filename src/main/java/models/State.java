c630d283dee7e4e5bd625127f1fb8442be8e611a
Gitari65 <agitari65@gmail.com>
2023-10-05 09:23:46 +0300

    Initial commit

---
+++ src/main/java/models/State.java
@@ -0,0 +1,25 @@
+package models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Entity
+@Data
+public class State {
+
+    @Id
+    private  Integer id;
+    private String name;
+    private String capital;
+    private String code;
+
+    @ManyToOne
+    @JoinColumn(name = "countryid",insertable = false,updatable = false)
+    private  Country country;
+    private Integer countryid;
+    private String details;
+
+}
