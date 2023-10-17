1e71cdbcfc3c326fa80bec2776d2241b8dc41a40
Gitari65 <agitari65@gmail.com>
2023-10-17 09:09:38 +0300

    controllers

--- src/main/java/com/example/manager/demo/controllers/VehicleMovementController.java
+++ src/main/java/com/example/manager/demo/controllers/VehicleMovementController.java
@@ -1,7 +1,12 @@
 package com.example.manager.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleMovementController {
+    @GetMapping("/vehiclemovement")
+    public  String getVehicleMovement(){
+        return "vehicleMovement";
+    }
 }
