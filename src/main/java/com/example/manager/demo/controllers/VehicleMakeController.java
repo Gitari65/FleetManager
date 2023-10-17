09edfee6d66f6dd5dc11fa4c75085160ff0bb488
Gitari65 <agitari65@gmail.com>
2023-10-17 09:09:13 +0300

    controllers

--- src/main/java/com/example/manager/demo/controllers/VehicleMakeController.java
+++ src/main/java/com/example/manager/demo/controllers/VehicleMakeController.java
@@ -1,4 +1,11 @@
 package com.example.manager.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class VehicleMakeController {
+    @GetMapping("/vehicleMake")
+    public  String getVehicleMake(){
+        return "vehicleMake";
+    }
 }
