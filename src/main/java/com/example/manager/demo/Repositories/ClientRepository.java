5f803cdc93b62e88d96bdf81fbde3290990dc311
Gitari65 <agitari65@gmail.com>
2023-10-19 14:25:39 +0300

    models

--- src/main/java/com/example/manager/demo/Repositories/ClientRepository.java
+++ src/main/java/com/example/manager/demo/Repositories/ClientRepository.java
@@ -2,8 +2,8 @@ package com.example.manager.demo.Repositories;
 
 import com.example.manager.demo.models.Client;
 
+
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.FluentQuery;
 import org.springframework.stereotype.Repository;
 
 
--- src/main/java/com/example/manager/demo/Repositories/ContactRepository.java
+++ src/main/java/com/example/manager/demo/Repositories/ContactRepository.java
@@ -1,9 +1,9 @@
 package com.example.manager.demo.Repositories;
 
-import com.example.manager.demo.models.Client;
+import com.example.manager.demo.models.Contact;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ContactRepository extends JpaRepository<Client, Integer> {
+public interface ContactRepository extends JpaRepository<Contact, Integer> {
 }
