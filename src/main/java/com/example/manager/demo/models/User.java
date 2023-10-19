a95fb11cb66fa10e67f926ce73c9b5cf8549125f
Gitari65 <agitari65@gmail.com>
2023-10-19 14:27:14 +0300

    models

--- src/main/java/com/example/manager/demo/models/User.java
+++ src/main/java/com/example/manager/demo/models/User.java
@@ -1,4 +1,19 @@
 package com.example.manager.demo.models;
 
-public class User {
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class,property = "id")
+
+public class User extends  CommonObject{
+
 }
