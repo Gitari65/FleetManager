4c65ff7ee1b883fb79ae337c82780aaf84c14e1c
Gitari65 <agitari65@gmail.com>
2023-10-19 14:26:05 +0300

    models

--- src/main/java/com/example/manager/demo/Repositories/JobTitleRepository.java
+++ src/main/java/com/example/manager/demo/Repositories/JobTitleRepository.java
@@ -1,9 +1,10 @@
 package com.example.manager.demo.Repositories;
 
-import com.example.manager.demo.models.Invoice;
+
+import com.example.manager.demo.models.JobTitle;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
+public interface JobTitleRepository extends JpaRepository<JobTitle, Integer> {
 }
