27094ca4f5b28b10fe1e28a6911c76033be73030
Gitari65 <agitari65@gmail.com>
2023-10-19 14:26:49 +0300

    models

---
+++ src/main/java/com/example/manager/demo/Repositories/StateRepository.java
@@ -0,0 +1,10 @@
+package com.example.manager.demo.Repositories;
+
+
+import com.example.manager.demo.models.State;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StateRepository extends JpaRepository<State, Integer> {
+}
