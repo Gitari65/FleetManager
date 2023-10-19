d532a273b3b4e3088ebde902612961e5535230fd
Gitari65 <agitari65@gmail.com>
2023-10-19 14:26:17 +0300

    models

---
+++ src/main/java/com/example/manager/demo/Repositories/LocationRepository.java
@@ -0,0 +1,9 @@
+package com.example.manager.demo.Repositories;
+
+import com.example.manager.demo.models.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+}
