ce28c3c93e6757e00fe68c9b00694077a68d81e6
Gitari65 <agitari65@gmail.com>
2023-10-20 21:56:57 +0300

    Repos

---
+++ src/main/java/com/example/manager/demo/Repositories/SupplierRepository.java
@@ -0,0 +1,9 @@
+package com.example.manager.demo.Repositories;
+
+import com.example.manager.demo.models.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
+}
