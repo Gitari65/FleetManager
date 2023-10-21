838d1da3260f963eb6a8fb4322dd633b3d0206b0
Gitari65 <agitari65@gmail.com>
2023-10-21 12:17:57 +0300

    Repos

--- src/main/java/com/example/manager/demo/controllers/SupplierController.java
+++ src/main/java/com/example/manager/demo/controllers/SupplierController.java
@@ -1,4 +1,11 @@
 package com.example.manager.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class SupplierController {
+    @GetMapping("/supplier")
+    public  String getSupplier(){
+        return "supplier";
+    }
 }
