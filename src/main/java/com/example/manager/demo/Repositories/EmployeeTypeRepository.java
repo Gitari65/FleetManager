58bb1d05640bad7d2d607054934c800a69ac1cea
Gitari65 <agitari65@gmail.com>
2023-10-21 12:17:33 +0300

    Repos

--- src/main/java/com/example/manager/demo/Repositories/EmployeeTypeRepository.java
+++ src/main/java/com/example/manager/demo/Repositories/EmployeeTypeRepository.java
@@ -1,7 +1,7 @@
 package com.example.manager.demo.Repositories;
 
-import com.example.manager.demo.models.Employee;
 import com.example.manager.demo.models.EmployeeType;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
