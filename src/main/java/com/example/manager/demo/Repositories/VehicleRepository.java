d44fcb49b88f5b83820b8a0d7e43beaee523f55f
Gitari65 <agitari65@gmail.com>
2023-10-22 21:29:48 +0300

    Repos

--- src/main/java/com/example/manager/demo/Repositories/VehicleRepository.java
+++ src/main/java/com/example/manager/demo/Repositories/VehicleRepository.java
@@ -1,4 +1,9 @@
 package com.example.manager.demo.Repositories;
 
-public class VehicleRepository {
+import com.example.manager.demo.models.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
 }
