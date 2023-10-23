5e3d3062c4e9647255af1814ff79f4febbd291f5
Gitari65 <agitari65@gmail.com>
2023-10-23 13:09:03 +0300

    Linking htMl pages

--- src/main/java/com/example/manager/demo/controllers/UserController.java
+++ src/main/java/com/example/manager/demo/controllers/UserController.java
@@ -1,5 +1,12 @@
 package com.example.manager.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class UserController {
+    @GetMapping("/users")
+    public String getUsers(){
+        return "user";
+    }
 
 }
