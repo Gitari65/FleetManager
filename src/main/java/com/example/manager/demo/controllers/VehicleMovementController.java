d735e7f85ca8776c0fe0248e64facf4a7fccebd2
Gitari65 <agitari65@gmail.com>
2023-10-23 13:10:18 +0300

    Linking htMl pages

--- src/main/java/com/example/manager/demo/controllers/VehicleMovementController.java
+++ src/main/java/com/example/manager/demo/controllers/VehicleMovementController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VehicleMovementController {
-    @GetMapping("/vehiclemovement")
+    @GetMapping("/vehicleMovement")
     public  String getVehicleMovement(){
         return "vehicleMovement";
     }
