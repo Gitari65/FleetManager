0221782059a6ae4c5908b2982b91abb23e3093a6
Gitari65 <agitari65@gmail.com>
2023-10-24 12:15:22 +0300

    Database -adding data

--- src/main/java/com/example/manager/demo/controllers/EmployeeTypeController.java
+++ src/main/java/com/example/manager/demo/controllers/EmployeeTypeController.java
@@ -1,4 +1,12 @@
 package com.example.manager.demo.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EmployeeTypeController {
+    @GetMapping("/employeeType")
+    public String getEmployeeType(){
+        return "employeeType";
+    }
 }
