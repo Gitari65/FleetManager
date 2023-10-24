2c5ec20757b028cdf7629f44d05a4c3bf584be9f
Gitari65 <agitari65@gmail.com>
2023-10-24 12:15:05 +0300

    Database -adding data

--- src/main/java/com/example/manager/demo/services/CountryService.java
+++ src/main/java/com/example/manager/demo/services/CountryService.java
@@ -1,5 +1,7 @@
 package com.example.manager.demo.services;
 
+import com.example.manager.demo.Repositories.CountryRepository;
+import com.example.manager.demo.models.Country;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.Entity;
@@ -9,11 +11,22 @@ import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CountryService {
 
-
+@Autowired
+    private CountryRepository countryRepository;
+public List<Country> getCountries(){
+    return countryRepository.findAll();
+}
+    //Add new Country
+    public void save(Country country){
+    countryRepository.save(country);
+}
 
 }
