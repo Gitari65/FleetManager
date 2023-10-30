899d6511e69c8018a544f9aef2dde6dab816e090
Gitari65 <agitari65@gmail.com>
2023-10-30 18:05:08 +0300

    edit behavior

--- src/main/java/com/example/manager/demo/controllers/CountryController.java
+++ src/main/java/com/example/manager/demo/controllers/CountryController.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CountryController {
     @Autowired
     private CountryService countryService;
-
-
 
     @GetMapping("/countries")
     public String getCountry(Model model){
@@ -31,6 +31,13 @@ public String addNew(Country country){
         countryService.save(country);
         return "redirect:/countries";
 }
+
+   @RequestMapping("/countries/findById")
+   @ResponseBody
+    public Optional<Country> findByID(int id){
+        return countryService.findById(id);
+
+    }
 
 
 }
