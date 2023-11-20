ae0256acf6a2cc5f24727888f736240fb6be9dce
Gitari65 <agitari65@gmail.com>
2023-11-20 17:50:58 +0300

    controlers

--- src/main/java/com/example/manager/demo/controllers/StateController.java
+++ src/main/java/com/example/manager/demo/controllers/StateController.java
@@ -1,11 +1,33 @@
 package com.example.manager.demo.controllers;
 
+
+import com.example.manager.demo.Repositories.StateRepository;
+import com.example.manager.demo.models.State;
+import com.example.manager.demo.services.StateService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.List;
+
 @Controller
 public class StateController {
+    @Autowired
+    private StateService stateService;
     @GetMapping("/state")
-    public  String getState(){
+    public  String getState(Model model){
+        List<State> stateList=stateService.getStates();
+        model.addAttribute("states",stateList);
         return "state";
     }
+    @RequestMapping(value = "/state/update",method = {RequestMethod.PUT,RequestMethod.POST})
+    public String updateStates(State state){
+        stateService.save(state);
+        return "redirect:/state";
+
+    }
+
 }
