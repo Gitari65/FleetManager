82a1b7bbf0695b425a9889794efff72804bbeda6
Gitari65 <agitari65@gmail.com>
2023-12-20 12:28:30 +0300

    car model

--- src/main/java/com/example/manager/demo/models/Car.java
+++ src/main/java/com/example/manager/demo/models/Car.java
@@ -1,2 +1,15 @@
-package com.example.manager.demo.models;public class Car {
+package com.example.manager.demo.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long carId;
+    @ManyToOne
+    @JoinColumn(name="model_id")
+    private CarModel carModel;
 }
