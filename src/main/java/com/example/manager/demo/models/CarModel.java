cda189c0572aa144ef5d6e346f83c0d826baabcd
Gitari65 <agitari65@gmail.com>
2024-01-10 09:31:02 +0300

    updates of user interface

--- src/main/java/com/example/manager/demo/models/CarModel.java
+++ src/main/java/com/example/manager/demo/models/CarModel.java
@@ -1,2 +1,17 @@
-package com.example.manager.demo.models;public class CarModel {
+package com.example.manager.demo.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Data
+@Entity
+public class CarModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long modelId;
+    private String name;
+
 }
