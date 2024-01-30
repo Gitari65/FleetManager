40502e69786816d4056dff0278272e60dea16b15
Gitari65 <agitari65@gmail.com>
2024-01-30 17:16:02 +0300

    models, services and repositories changes

--- src/main/java/com/example/manager/demo/models/User.java
+++ src/main/java/com/example/manager/demo/models/User.java
@@ -5,12 +5,17 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long userId;
     private String username;
     private String password;
+    private String email;
     @Enumerated(EnumType.STRING)
-    private UserRole role;
+    private Role role;
 }
