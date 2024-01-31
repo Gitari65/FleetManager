415367507be0bfaf4f20710c3c3df308185a6aef
Gitari65 <agitari65@gmail.com>
2024-01-31 14:43:27 +0300

    adding spring security dependency

---
+++ src/main/java/com/example/manager/demo/Repositories/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.example.manager.demo.Repositories;
+
+import com.example.manager.demo.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,> {
+}
