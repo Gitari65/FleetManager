5b41cc481df2828a29ceb5d17df269dffe427842
Gitari65 <agitari65@gmail.com>
2024-01-31 14:45:12 +0300

    sigin controller messages

--- src/main/java/com/example/manager/demo/controllers/VehicleController.java
+++ src/main/java/com/example/manager/demo/controllers/VehicleController.java
@@ -9,33 +9,42 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import javax.sql.rowset.serial.SerialBlob;
 import java.io.IOException;
+import java.sql.Blob;
+import java.sql.SQLException;
 import java.util.List;
 
 @Controller
+@RequestMapping("/car")
 public class VehicleController {
     @Autowired
     private VehicleService vehicleService;
 
 
 
-    public String deleteVehicle(Long id){
-        vehicleService.deleteVehicle(id);
-        return "redirect:/newVehicles";
+//    public String deleteVehicle(Long id){
+//        vehicleService.deleteVehicle(id);
+//        return "redirect:/newVehicles";
+//
+//    }
+@PostMapping("/add")
+public String addNewVehicle(@ModelAttribute Car car, @RequestParam("image") MultipartFile imageFile) throws IOException {
 
-    }
-    @PostMapping("/newVehicle")
-    public String addNewVehicle(@ModelAttribute Car car, @RequestParam("image")MultipartFile imageFile)throws IOException {
-        car.setImage(imageFile.getBytes());
-        vehicleService.addNewVehicle(car);
-
-        return "redirect:/newVehicle";
-    }
-
-
+    vehicleService.addNewVehicle(car,imageFile);
+    return "redirect:/newVehicles";
+}
 
 
 }
+
+
+
+
+
+
+
