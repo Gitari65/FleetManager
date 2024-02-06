669a67a7ba06ca648fed38f37eedb720e8cc68c4
Gitari65 <agitari65@gmail.com>
2024-02-06 12:42:08 +0300

    models, services and repositories changes

--- src/main/java/com/example/manager/demo/services/CarMakeService.java
+++ src/main/java/com/example/manager/demo/services/CarMakeService.java
@@ -1,19 +1,31 @@
 package com.example.manager.demo.services;
 
 import com.example.manager.demo.Repositories.MakeRepository;
+import com.example.manager.demo.Repositories.VehicleRepository;
+import com.example.manager.demo.models.Car;
 import com.example.manager.demo.models.CarMake;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
+import java.util.UUID;
+
 
 @Service
 public class CarMakeService {
     @Autowired
     private MakeRepository makeRepository;
+    private VehicleRepository vehicleRepository;
 
     public List<CarMake> getCarMakes(){
         return makeRepository.findAll();
     }
+
 
 }
