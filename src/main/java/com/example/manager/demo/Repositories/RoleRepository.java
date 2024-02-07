d8a2c960e606174bdc3c3149d72b8d8b0eaf3875
Gitari65 <agitari65@gmail.com>
2024-02-07 14:52:41 +0300

    syling the vehicle page

--- src/main/java/com/example/manager/demo/Repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.manager.demo.Repositories;
-
-import com.example.manager.demo.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role,> {
-}
