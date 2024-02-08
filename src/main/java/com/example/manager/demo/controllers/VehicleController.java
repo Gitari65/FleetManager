9771920b9746ddb311c3a3c1ba8d5dbc342cbded
Gitari65 <agitari65@gmail.com>
2024-02-08 21:54:22 +0300

    models, services and repositories changes

--- src/main/java/com/example/manager/demo/controllers/VehicleController.java
+++ src/main/java/com/example/manager/demo/controllers/VehicleController.java
@@ -7,10 +7,7 @@ import com.example.manager.demo.services.VehicleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.sql.rowset.serial.SerialBlob;
@@ -19,7 +16,7 @@ import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/car")
 public class VehicleController {
     @Autowired
@@ -32,17 +29,18 @@ public class VehicleController {
 //        return "redirect:/newVehicles";
 //
 //    }
-//@PostMapping("/add")
-//public String addNewVehicle(@ModelAttribute Car car, @RequestParam("image") MultipartFile imageFile) throws IOException {
-//
-//    try {
-//     vehicleService.saveCar(car, imageFile);
-//        return "redirect:/newVehicles";
-//    } catch (IOException e) {
-//        e.printStackTrace();
-//        return "error";
-//    }
-//}
+@PostMapping("/add")
+public String addNewVehicle(@ModelAttribute Car car) throws IOException {
+
+    try {
+//        , @RequestParam("image") MultipartFile imageFile
+     vehicleService.saveCar(car);
+        return "redirect:/newVehicle";
+    } catch (IOException e) {
+        e.printStackTrace();
+        return "error";
+    }
+}
 
 
 }
