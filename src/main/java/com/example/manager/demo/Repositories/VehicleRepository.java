b5a9b5fc108d959191fa5d878d4a8362e31e49b8
Gitari65 <agitari65@gmail.com>
2024-02-26 14:08:37 +0300

    api documentation using open api swagger-ui

--- src/main/java/com/example/manager/demo/Repositories/VehicleRepository.java
+++ src/main/java/com/example/manager/demo/Repositories/VehicleRepository.java
@@ -4,8 +4,14 @@ import com.example.manager.demo.models.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface VehicleRepository extends JpaRepository<Car,Long> {
+
+            public  List<Car> findByCarModel_CarMake_MakeId(Long makeId);
+            public List<Car> findByCarModel_ModelId(Long modelId);
 
 
 }
