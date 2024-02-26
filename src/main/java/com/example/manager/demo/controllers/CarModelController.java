21ea0109354a93fee5a4a3b4eb843769bbc0f384
Gitari65 <agitari65@gmail.com>
2024-02-26 14:07:15 +0300

    implementing an endpoint  for adding and updating vehicles as per model

--- src/main/java/com/example/manager/demo/controllers/CarModelController.java
+++ src/main/java/com/example/manager/demo/controllers/CarModelController.java
@@ -2,11 +2,14 @@ package com.example.manager.demo.controllers;
 
 import com.example.manager.demo.models.CarModel;
 import com.example.manager.demo.services.CarModelService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.swing.text.html.HTML;
 import java.util.List;
 @Controller
 @RequestMapping("/carModels")
@@ -15,6 +18,8 @@ public class CarModelController {
     private CarModelService carModelService;
     /**Get car models**/
     /**GET car models by make id**/
+    @Tag(name="Get"  )
+    @Operation(summary = "Get car models by make id")
     @GetMapping("/ByMakeId/{carMakeId}")
     @ResponseBody
     public List<CarModel> getCarModelsByMakeId(@PathVariable  Long carMakeId){
