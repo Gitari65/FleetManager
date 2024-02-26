72bd901a37d8b32c0cf06f714a94a3d4bcdad689
Gitari65 <agitari65@gmail.com>
2024-02-26 14:07:59 +0300

    implementing an endpoint  for adding and updating vehicles as per car model and make

--- src/main/java/com/example/manager/demo/controllers/VehicleController.java
+++ src/main/java/com/example/manager/demo/controllers/VehicleController.java
@@ -4,6 +4,8 @@ import com.example.manager.demo.models.Car;
 import com.example.manager.demo.models.CarMake;
 import com.example.manager.demo.models.CarModel;
 import com.example.manager.demo.services.VehicleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +17,7 @@ import java.io.IOException;
 import java.sql.Blob;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/car")
@@ -24,23 +27,57 @@ public class VehicleController {
 
 
 
-//    public String deleteVehicle(Long id){
-//        vehicleService.deleteVehicle(id);
-//        return "redirect:/newVehicles";
-//
-//    }
-@PostMapping("/add")
-public String addNewVehicle(@ModelAttribute Car car) throws IOException {
+        @Tag(name= "Post")
+        @Operation(summary = "Add new vehicle")
+        @PostMapping("/add")
+        public String addNewVehicle(@ModelAttribute Car car) throws IOException {
 
-    try {
-//        , @RequestParam("image") MultipartFile imageFile
-     vehicleService.saveCar(car);
-        return "redirect:/newVehicle";
-    } catch (IOException e) {
-        e.printStackTrace();
-        return "error";
-    }
-}
+            try {
+        //        , @RequestParam("image") MultipartFile imageFile
+             vehicleService.saveCar(car);
+                return "redirect:/newVehicle";
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "error";
+            }
+        }
+        @Tag(name="Post" )
+        @Operation(summary = "Delete vehicle by id")
+        @PostMapping("/delete")
+         public String deleteVehicleById(@RequestParam Long id) {
+            vehicleService.deleteVehicle(id);
+            return "redirect:/newVehicle";
+        }
+
+        @Tag(name="Post")
+        @Operation(summary = "Update vehicle")
+        @PostMapping("/update")
+        public String updateVehicle(@ModelAttribute Car car,Long carId) throws IOException {
+            vehicleService.updateVehicle(carId,car);
+            return "redirect:/newVehicle";
+        }
+        @Tag(name="Get" )
+        @Operation(summary = "Get all vehicles")
+        @GetMapping("/all")
+        public List<Car> getAllVehicles() {
+            return vehicleService.getAllVehicles();
+        }
+
+        @Tag(name="Get" )
+        @Operation(summary = "Get vehicle by model id")
+        @GetMapping("/ByModelId/{carModelId}")
+        public List<Car> getVehiclesByModelId(@PathVariable Long carModelId) {
+            return vehicleService.getVehiclesByModelId(carModelId);
+        }
+
+        @Tag(name="Get" )
+        @Operation(summary = "Get vehicle by make id")
+        @GetMapping("/ByMakeId/{carMakeId}")
+        public List<Car> getVehiclesByMakeId(@PathVariable Long carMakeId) {
+            return vehicleService.getVehiclesByMakeId(carMakeId);
+        }
+
+
 
 
 }
